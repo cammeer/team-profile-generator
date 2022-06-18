b89338f2ecccc20f63a82f26fee3aeadd542e0c9
cardinal <caren@merzhaus.org>
2022-06-18 13:53:57 -0400

    added more questions; inquirer working but not pushing

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-const { writeFile, copyFile } = require('./utils/generate-site.js');
+// const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
+// const generatePage = require('./src/page-template');
+
+
+const questions = [{
+        type: "input",
+        name: "createTeam",
+        message: "Let's create your team! Enter employee info to create cards and render an HTML page. Press Enter to Begin."
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is their employee ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email address?"
+    },
+    {
+        type: "list",
+        message: "Select team member's role by using the down arrow and pressing Enter",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern", "Employee"]
+    },
+
+];
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+}
+// Function call to initialize app
+init();
+
+
 
 // "data" is the team arrary (re: ryan)
 
