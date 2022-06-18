9dc1cdb3cf67c3779f02f3c38ec5a3e29ce15869
cardinal <caren@merzhaus.org>
2022-06-18 13:17:48 -0400

    create the rest of the roles

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+
+// "data" is the team arrary
+// const generateCards = (data) =>{
+//     data.forEach((employee)=>{
+//       let role = employee.getRole()
+//       if(role ==="Manager"){
+//           const managerCard = generateManager(employee);
+//           cardArr.push(managerCard);
+//       }
+//       if(role ==="Engineer"){
+//           const engineerCard = generateEngineer(employee);
+//           cardArr.push(engineerCard);
+//       }
+//       if(role === "Intern"){
+//           const internCard = generateIntern(employee);
+//           cardArr.push(internCard);
+//       }
+//     })
+//     let cardStr = cardArr.join("");
+//     const renderPage = renderHTML(cardStr);
+//     return renderPage
+// };
