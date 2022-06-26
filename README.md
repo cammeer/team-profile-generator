67ac8176fd95a1a7458ab9d8bc2a1b778318a061
cardinal <caren@merzhaus.org>
2022-06-26 13:07:20 -0400

    added readme, backed outsome other changes

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# team-profile-generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# Team Profile Generator
+  ## Description
+  This is a javascript application that automatically generates a website for your project team or company. It was created using the inqurirer package.
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  
+  ## Installation
+  Installation instructions will be provided when this application has been updated.
+  
+  ## Usage
+  Usage instructions will be provided soon! Here's a link to a video showing basic operation: [Google Drive Link](https://drive.google.com/)
+  ## License
+ Copyright © 2022 [Caren Merz](https://github.com/cammeer). <br />
+This project is [MIT](https://github.com/cammeer/next-progress-bar/blob/main/LICENSE) licensed.
+  ## Contributions
+  I was the only contributor for this project so far. If you would like to contribute, see my email below.
+  
+  ## Testing
+  N/A
+  
+  ## Any Questions
+  This project was created by me: [cammeer](https://github.com/cammeer)
+  
+  Contact me with any questions at: [caren@merzhaus.org](caren@merzhaus.org)
--- src/page-template.js
+++ src/page-template.js
@@ -1,100 +1,36 @@
-// const employee = require("../lib/employee")
-
-const mquestions = manager => {
+const generateManager = mgrText => {
     return `
-    <div id="${manager.getRole()}-card" class="box card">
-      <div class="box name-role manager-name">
-        <h2>${manager.getName()}</h2>
-        <h3>Role: ${manager.getRole()}</h3>
-      </div> 
-      <div class="box employee-info">
-        <ul class="list-group">
-          <li class="list-group-item">ID: ${manager.getId()}</li>
-          <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-          <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
-        </ul>
+    <p>${mgrText}</p>
+    `;
+}
+
+// export function to generate entire page
+module.exports = templateData => {
+    // destructure page data by section
+    const { manager, engineer, intern, ...header } = templateData;
+
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E Corp Employee Site</title>
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">E Corp's Evil Employees</h1>
       </div>
-    </div>
-  `
-}
-
-// const equestions = engineer => {
-//         return `
-//     <div id="${engineer.getRole()}-card" class="box card">
-//       <div class="box name-role engineer-name">
-//         <h2>${engineer.getName()}</h2>
-//         <h3>Role: ${engineer.getRole()}</h3>
-//       </div> 
-//       <div class="box employee-info">
-//         <ul class="list-group">
-//           <li class="list-group-item">ID: ${engineer.getId()}</li>
-//           <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-
-//           <li class="list-group-item">Github:
-//           <a href="https://www.github.com/${engineer.getGithub()}" target="_blank">www.github.com/${engineer.getGithub()}</a>
-//           </li>
-//         </ul>
-//       </div>
-//     </div>
-//   `
-//     }
-// Intern card template
-// const iquestions = intern => {
-//         return `
-//     <div id="${intern.getRole()}-card" class="box card">
-//       <div class="box name-role intern-name">
-//         <h2>${intern.getName()}</h2>
-//         <h3>Role: ${intern.getRole()}</h3>
-//       </div> 
-//       <div class="box employee-info">
-//         <ul class="list-group">
-//           <li class="list-group-item">ID: ${intern.getId()}</li>
-//           <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-//           <li class="list-group-item">School: ${intern.getSchool()}</li>
-//         </ul>
-//       </div>
-//     </div>
-//   `
-//     }
-// create and combine cards
-const employeesDiv = employee => {
-    let employeeHtml = ''
-
-    for (i = 0; i < employee.length; i++) {
-        if (data[i].getRole() === "manager") {
-            employeeHtml = employeeHtml + manager(mquestions[i])
-        }
-        if (data[i].getRole() === "engineer") {
-            employeeHtml = employeeHtml + engineer(equestions[i])
-        }
-        if (data[i].getRole() === "intern") {
-            employeeHtml = employeeHtml + intern(iquestions[i])
-        }
-    }
-    return employeeHtml
-}
-
-// html boilerplate
-const template = data => {
-    return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>E Corp Team Site</title>
-        <link href="bootstrap link goes here">
-        <link rel="stylesheet" href="./style.css"><source src="../page-template.js" type="page-template">
-      </head>
-      <body>
-        <header class = "container">
-          <div class = "flex box justify-center my-team">
-            <h1> E Corp's Evil Employees </h1>
-          </div>
-        </header>
-        <main class = "container">
-        </main>
-      </body>
-    </html>
-  `
-}
-
-module.exports = template;
+    </header>
+    <main class="container my-5">
+    </main>
+  </body>
+  </html>
+  `;
+};
