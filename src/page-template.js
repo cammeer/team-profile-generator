4b2ee4db67b11ad0cf45f923ca88f4553d2d44ff
cardinal <caren@merzhaus.org>
2022-06-26 13:10:14 -0400

    added bootstrap

--- src/page-template.js
+++ src/page-template.js
@@ -17,8 +17,7 @@ module.exports = templateData => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>E Corp Employee Site</title>
-    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js">
-    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
   </head>
 
@@ -28,8 +27,9 @@ module.exports = templateData => {
         <h1 class="page-title text-secondary bg-dark py-2 px-3">E Corp's Evil Employees</h1>
       </div>
     </header>
-    <main class="container my-5">
+    <main class="">
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
   </html>
   `;
